5b457de4c2c1b75ddc6c58417c653a2ba5b843d0
Dylan Michael Labat <dml252@live.com>
2019-01-14 15:11:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+
+def introduction_with_name(name, lang)
+  puts "Hi, my name is #{name} and I am learning to program in #{lang}."
+end
